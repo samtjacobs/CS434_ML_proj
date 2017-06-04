f7af20172831ead2c68e9803f306624a0ced0265
samtjacobs <jacobssa@oregonstate.edu>
2017-06-03 21:59:41 -0700

    Got padding working

---
+++ test/vec_test.py
@@ -0,0 +1,10 @@
+from keras.preprocessing.sequence import pad_sequences
+import numpy as np
+seq1 = np.array([[1,1,1],[2,2,2],[3,3,3]])
+seq2 = np.array([[2,2,2],[3,3,3]])
+seq3 = np.array([[3,3,3]])
+seqs = [seq1, seq2, seq3]
+
+padded = pad_sequences(seqs, maxlen=None, dtype='int32',padding='pre', truncating='pre', value=0.)
+
+print(str(seqs))
