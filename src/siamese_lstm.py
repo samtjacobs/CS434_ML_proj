378ae5e4c9c7218ad15eaa5d1b6febe85ed74871
samtjacobs <jacobssa@oregonstate.edu>
2017-06-04 15:16:59 -0700

    Compiles

--- src/siamese_lstm.py
+++ src/siamese_lstm.py
@@ -3,12 +3,22 @@
 import numpy as np
 from keras.models import Sequential, Model
 from keras.preprocessing.sequence import pad_sequences
-from keras.layers import Dense, Dropout, Input, Lambda, LSTM, merge
-from keras.layers.core import Masking
+from keras.layers import Dense, Dropout, Input, Lambda, LSTM, merge, Merge
 from keras.optimizers import RMSprop
 from keras.layers.merge import Dot
+from keras import backend as K
 
 maxlen = 30
+
+def cosine_distance(vests):
+    x, y = vests
+    x = K.l2_normalize(x, axis=-1)
+    y = K.l2_normalize(y, axis=-1)
+    return -K.mean(x * y, axis=-1, keepdims=True)
+
+def cos_dist_output_shape(shapes):
+    shape1, shape2 = shapes
+    return (shape1[0],1)
 
 def load_training_data():
     trs = np.load('../preproc/train.npy')
@@ -18,13 +28,13 @@ def load_training_data():
 
 
 #using TensorFlow backend
-def create_LSTM():
+def create_LSTM(input):
     seq = Sequential()
     # add masking layer to ignore empty words in sentence list
     #seq.add(Masking(mask_value=0., input_shape=(timesteps, features)))
     #need to decide on LSTM arguments still, probably most will be default, except maybe activation would be better relu than linear?
 
-    seq.add(LSTM(32, input_shape=(maxlen,300)))
+    seq.add(LSTM(32, input_shape=input))
     seq.add(Dense(32))
     #also need to decide whether to add more LSTM layers, dense layers, how many... not sure what the intuition for this is to narrow choices,
     #so that I can then start testing effects on prediction accuracy
@@ -48,28 +58,23 @@ def main():
     q1_word_embeddings = pad_sequences(q1, maxlen=maxlen, dtype='float32',padding='pre', truncating='pre', value=0.)
     q2_word_embeddings = pad_sequences(q2, maxlen=maxlen, dtype='float32',padding='pre', truncating='pre', value=0.)
     #create base LSTM models
-    sibling_LSTM = create_LSTM()
+    input_q1 = Input(shape=(maxlen,300),dtype='float32',name='q1')
+    input_q2 = Input(shape=(maxlen,300),dtype='float32',name='q2')
+    siamese_LSTM = create_LSTM((maxlen,300))
 
-    #I don't know until later today how my partner's word embeddings are formatted, and how to give them to this input layer, and whether dimensions should be specified
-    #q1_word_embeddings = Input()#specify input shape as parameter
-    #q2_word_embeddings = Input()
-    first_question = sibling_LSTM()
-    second_question = sibling_LSTM()
+    question_1 = siamese_LSTM(input_q1)
+    question_2 = siamese_LSTM(input_q2)
+    distance = Lambda(cosine_distance, output_shape=cos_dist_output_shape)([question_1, question_2])
 
-    merger = Dot(normalize=True)
-    model = merge(input=[first_question, second_question], output=merger)#cosine similarity for now, would like to try KL-divergence later
-
-    # train
-
+    model = Model(input=[input_q1, input_q2], output=distance)
     #compile with mean squared error and use RMSprop (generally good for recurrent networks)
     model.compile(loss='mean_squared_error', optimizer='RMSprop')
 
     #again the formatting of the data will be important here, and parameters will change, but I'm just looking for something simple to do parameter testing with
-    model.fit([q1_data, q2_data], labels, validation_split=.20,
+    #model.fit([q1_word_embeddings, q2_word_embeddings], gt)
+    model.fit([q1_word_embeddings, q2_word_embeddings], gt, validation_split=.20,
               batch_size=100, verbose=2, nb_epoch=10)
-
+    model.save('quora_regressor.h5')
     # compute final accuracy on training and test sets
-    training_predictions = model.predict(train_data)
-    test_predictions = model.predict(test_data)
 
 main()
