232e463a4b049bbd23cef9fbc840003afb87f830
samtjacobs <jacobssa@oregonstate.edu>
2017-06-13 03:15:58 -0700

    Added to readme

--- README.md
+++ README.md
@@ -7,5 +7,18 @@ python preproc.py <data_set_path> <word_vectors_path>
 ```
 Expects a .txt, e.g. ```glove.6B.300.txt```.  Writes pickles.
 
-## Source
+## Training
+Simple, if directory structure unchanged due to hardcoded paths.
+```
+python train.py
+```
+Saves model and weights.  Uses time of day to create name.
+
+## Results
+Seen in:
+![alt text][results]
+
+## Sources
 I borrowed *alot* of how to handle nlp data from [this tutorial](https://blog.keras.io/using-pre-trained-word-embeddings-in-a-keras-model.html) on Keras.
+
+[results]: https://github.com/samtjacobs/CS434_ML_proj/blob/master/val_acc80.png "Results of 10 epochs and ~300k samples"
---
+++ eval/eval.py
@@ -0,0 +1,118 @@
+#This is a basic model that will be used with word embeddings for question semantic similarity predictions
+
+import numpy as np
+import h5py as h5py
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+from keras.utils.np_utils import to_categorical
+from keras.layers import Dense, Dropout, Input, Lambda, TimeDistributed, BatchNormalization, concatenate
+from keras.optimizers import Adam
+from keras.layers import Embedding
+from keras import backend as K
+from keras.models import Model
+import csv
+import cPickle as pickle
+import time
+from keras.callbacks import Callback, ModelCheckpoint
+
+
+maxlen = 30
+MAX_NB_WORDS = 200000
+PREPROC_PATH = '../preproc/'
+EMBEDDING_DIM = 300
+HIDDEN_DIM = 100
+DROPOUT = 0.2
+MAX_SEQUENCE_LENGTH = 64
+VALIDATION_SPLIT = 0.90
+MNAME = 'quora_mod'
+
+def make_tokens(question1, question2):
+    questions = question1 + question2
+    tokenizer = Tokenizer(nb_words=MAX_NB_WORDS)
+    tokenizer.fit_on_texts(questions)
+    return tokenizer
+
+
+def get_embed_weights(word_ind, embed_ind):
+    embedding_matrix = np.zeros((len(word_ind) + 1, EMBEDDING_DIM))
+    for word, i in word_ind.items():
+        embedding_vector = embed_ind.get(word)
+        if embedding_vector is not None:
+            # words not found in embedding index will be all-zeros.
+            embedding_matrix[i] = embedding_vector
+    return embedding_matrix
+
+
+def make_embed_layer(word_ind, embed_w):
+    return Embedding(len(word_ind) + 1,
+                    EMBEDDING_DIM,
+                    weights=[embed_w],
+                    input_length=MAX_SEQUENCE_LENGTH,
+                    trainable=False)
+
+
+def main():
+    word_ind = pickle.load(open(PREPROC_PATH + 'word_ind.p'))
+    embedding_ind = pickle.load(open(PREPROC_PATH + '6B_word_vec.p'))
+    q_seqs = pickle.load(open(PREPROC_PATH + 'que_seqs.p'))
+    gt = pickle.load(open(PREPROC_PATH + 'labels.p'))
+    q1_seq = q_seqs[0]
+    q2_seq = q_seqs[1]
+    q1_data = pad_sequences(q1_seq, maxlen=MAX_SEQUENCE_LENGTH)
+    q2_data = pad_sequences(q2_seq, maxlen=MAX_SEQUENCE_LENGTH)
+
+    #labels = to_categorical(np.asarray(gt, dtype=int))
+    labels = np.asarray(gt, dtype='float32')
+ #  indices = np.arange(q1_data.shape[0])
+ #   np.random.shuffle(indices)
+ #   q1_data = q1_data[indices]
+ #   q2_data = q2_data[indices]
+ #   labels = labels[indices]
+    nb_validation_samples = int(VALIDATION_SPLIT * q1_data.shape[0])
+
+    # Seperate testing and validation sets
+    q1_train = q1_data[:-nb_validation_samples]
+    q2_train = q2_data[:-nb_validation_samples]
+    l_train = labels[:-nb_validation_samples]
+    q1_val = q1_data[-nb_validation_samples:]
+    q2_val = q2_data[-nb_validation_samples:]
+    l_val = labels[-nb_validation_samples:]
+
+    # Make model.  Siamese.  Two legs, one for each question.  Share embedding
+# and LSTM layers
+# Note: I am using the Keras functional API, not sequential or graph
+    embed_w = get_embed_weights(word_ind, embedding_ind)
+    q1 = Input(shape=(MAX_SEQUENCE_LENGTH, ))
+    q2 = Input(shape=(MAX_SEQUENCE_LENGTH, ))
+    embed1 = make_embed_layer(word_ind, embed_w)
+    embed2 = make_embed_layer(word_ind, embed_w)
+    # Might add mask
+    q1_path = embed1(q1)
+    q2_path = embed2(q2)
+    # TimeDistributed is 2D LSTM from my understanding
+    q1_path = TimeDistributed(Dense(EMBEDDING_DIM, activation='relu'))(q1_path)
+    q2_path = TimeDistributed(Dense(EMBEDDING_DIM, activation='relu'))(q2_path)
+    # MaxPooling Layer.  May add additional dense nueral net before it
+    q1_path = Lambda(lambda x: K.max(x, axis=1), output_shape=(EMBEDDING_DIM,))(q1_path)
+    q2_path = Lambda(lambda x: K.max(x, axis=1), output_shape=(EMBEDDING_DIM,))(q2_path)
+    # Merge w/ concatenation
+    merged = concatenate([q1_path, q2_path])
+    # Pass thru single path
+    merged = Dense(200, activation='relu')(merged)
+    merged = Dropout(DROPOUT)(merged)
+    merged = BatchNormalization()(merged)
+    merged = Dense(200, activation='relu')(merged)
+    merged = Dropout(DROPOUT)(merged)
+    merged = BatchNormalization()(merged)
+    # Final
+    pred = Dense(1, activation='sigmoid')(merged)
+
+    model = Model(inputs=[q1, q2], output=pred)
+    TIME = str(time.localtime().tm_hour) + str(time.localtime().tm_min)
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+    model.load_weights(MNAME + '_' + TIME +'_weights.h5')
+    predictions = model.predict([q1_data, q2_data], batch_size=100, verbose=0)
+    np.save(open(TIME + '_' + "predictions.np", "wb"), predictions)
+
+if __name__ == "__main__":
+    main()
